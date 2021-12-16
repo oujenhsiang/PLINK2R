e346e1c9263d52d95993f5a9e3e9522d702eb99a
Jen-Hsiang Ou <oumark.me@outlook.com>
2021-12-16 10:46:07 +0100

    version1

--- R/readPLINK.R
+++ R/readPLINK.R
@@ -1,3 +1,22 @@
 readPLINK = function(prefix){
 
+  # INDS
+  INDS = fread(paste(prefix, ".fam", sep = "")) %>%
+    as_tibble() %>%
+    rename(fam = V1, id = V2, pat = V3, mat = V4, sex = V5, pheno = V6)
+  fam$pat[fam$pat == 0] = NA
+  fam$mat[fam$mat == 0] = NA
+  fam$sex[fam$sex == 0] = NA
+  fam$pheno[fam$pheno == -9] = NA
+
+  # SNPS
+  SNPS = fread(paste(prefix, ".bim", sep = "")) %>%
+    as_tibble() %>%
+    rename(chr = V1, id = V2, posg = V3, pos = V4, ref = V5, alt = V6)
+
+  # GENO
+  GENO = t(read_bed(prefix, names_loci = SNPS$id, names_ind = INDS$id))
+
+  # return results
+  return(list(GENO, SNPS, INDS))
 }
